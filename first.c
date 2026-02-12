9016630a7f3f3a4e1a0db609894045fc8505369f
lucifer34263 <25114cn363@glbitm.ac.in>
2026-02-12 09:59:53 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ int x,y,z;
+printf("I created a new GITHUB accnt today\n");
+printf("Enter the values of x:");
+scanf("%d",&x);
+printf("Enter the values of y:");
+scanf("%d",&y);
+z=x+y;
+printf("The sum of x and y is =%d",z);
+return 0;
+}
