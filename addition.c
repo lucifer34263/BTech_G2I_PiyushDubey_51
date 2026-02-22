4a15274e8835b7e38e4c667a6dfef17d07018291
lucifer34263 <25114cn363@glbitm.ac.in>
2026-02-22 15:27:07 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a,b,c;
+printf("enter the first number:");
+scanf("%d",&a);
+printf("enter the second number:");
+scanf("%d",&b);
+c=a+b;
+printf("The addition of two numbers is:%d",c);
+return 0;
+
+}
